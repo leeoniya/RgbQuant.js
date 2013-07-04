ef5bf2cecdf00edc424a64b4ce40bd9b633935d4
Leon Sorokin <leeoniya@gmail.com>
2013-07-04 14:25:12 -0500

    let the public shaming begin!

---
+++ rgbquant.js
@@ -0,0 +1,529 @@
+/*
+* Copyright (c) 2013, Leon Sorokin
+* All rights reserved. (MIT Licensed)
+*
+* rgbquant.js - image color quantizer
+*/
+
+(function(){
+	function RgbQuant(opts) {
+		opts = opts || {};
+
+		this.method = opts.method || 1;
+		// desired final palette size
+		this.colors = opts.colors || 256;
+		// # of highest-frequency colors to start with for palette reduction
+		this.initColors = opts.initColors || 4096;
+		// color-distance threshold for initial reduction pass
+		this.initDist = (opts.initDist || opts.initDist === 0) ? opts.initDist : .05;
+		// subsequent passes threshold
+		this.distIncr = opts.distIncr || 0.02;
+		// palette grouping
+		this.hueGroups = opts.hueGroups || 8;
+		// subregion partitioning box size
+		this.boxSize = opts.boxSize || [64,64];
+		// number of same pixels required within box for histogram inclusion
+		this.boxPxls = opts.boxPxls || 2;
+		// palette locked indicator
+		this.palLocked = false;
+		// palette sort order
+//		this.sortPal = ['hue-','lum-','sat-'];
+
+		// accumulated histogram
+		this.histogram = {};
+		// palette - rgb triplets
+		this.idxrgb = [];
+		// palette - int32 vals
+		this.idxi32 = [];
+		// reverse lookup {i32:idx}
+		this.i32idx = {};
+		// remap lookup {i32old:i32new}
+		this.i32i32 = {};
+		// {i32:rgb}
+		this.i32rgb = {};
+	}
+
+	// gathers histogram info
+	RgbQuant.prototype.sample = function sample(img, width) {
+		if (this.palLocked)
+			throw "Cannot sample additional images, palette already assembled.";
+
+		var data = getImageData(img, width);
+
+		switch (this.method) {
+			case 1: this.colorStats1D(data.buf32); break;
+			case 2: this.colorStats2D(data.buf32, data.width); break;
+		}
+	};
+
+	// image quantizer
+	RgbQuant.prototype.reduce = function(img, indexed) {
+		if (!this.palLocked)
+			this.buildPal();
+
+		var data = getImageData(img),
+			buf32 = data.buf32,
+			len = buf32.length,
+			out = indexed ? new Uint8Array(len) : new Uint32Array(len);
+
+		for (var i = 0; i < len; i++) {
+			var i32 = buf32[i];
+			out[i] = indexed ? this.nearestIndex(i32) : this.nearestColor(i32);
+		}
+
+		return indexed ? out : new Uint8Array(out.buffer);
+	};
+
+	// reduces histogram to palette, remaps & memoizes reduced colors
+	RgbQuant.prototype.buildPal = function buildPal() {
+		var histG  = this.histogram,
+			sorted = sortedHashKeys(histG, true);
+
+		if (sorted.length == 0)
+			throw "Nothing has been sampled, palette cannot be built.";
+
+		switch (this.method) {
+			case 1:
+				var cols = this.initColors,
+					last = sorted[cols - 1],
+					freq = histG[last];
+
+				var idxi32 = sorted.slice(0, cols);
+
+				var pos = cols, len = sorted.length;
+				while (pos < len && histG[sorted[pos]] == freq)
+					idxi32.push(sorted[pos++]);
+				break;
+			case 2:
+				var idxi32 = sorted;
+				break;
+		}
+
+		// int32-ify values
+		idxi32 = idxi32.map(function(v){return +v;});
+
+		this.reducePal(idxi32);
+		this.sortPal();
+		this.palLocked = true;
+	};
+
+	// reduces similar colors from an importance-sorted Uint32 rgba array
+	RgbQuant.prototype.reducePal = function reducePal(idxi32) {
+		// build full rgb palette
+		var idxrgb = idxi32.map(function(i32) {
+			return [
+				(i32 & 0xff),
+				(i32 & 0xff00) >> 8,
+				(i32 & 0xff0000) >> 16,
+			];
+		});
+
+		var len = idxrgb.length,
+			palLen = len,
+			i32i32 = {};
+			thold = this.initDist;
+
+		// palette already at or below desired length
+		if (palLen > this.colors) {
+			while (palLen > this.colors) {
+				var memDist = [];
+
+				// iterate palette
+				for (var i = 0; i < len; i++) {
+					var pxi = idxrgb[i], i32i = idxi32[i];
+					if (!pxi) continue;
+
+					for (var j = i + 1; j < len; j++) {
+						var pxj = idxrgb[j], i32j = idxi32[j];
+						if (!pxj) continue;
+
+						var dist = colorDist(pxi, pxj) * 100;
+
+						if (dist < thold) {
+							// store index,rgb,dist
+							memDist.push([j, pxj, i32j, dist]);
+
+							// remap & kill squashed value
+							i32i32[i32j] = i32i;
+							delete(idxrgb[j]);
+							palLen--;
+						}
+					}
+				}
+
+				thold += this.distIncr;
+			}
+
+			// if palette is over-reduced, re-add removed colors with largest distances from last round
+			if (palLen < this.colors) {
+				// sort descending
+				sort.call(memDist, function(a,b) {
+					return b[3] - a[3];
+				});
+
+				var k = 0;
+				while (palLen < this.colors) {
+					// re-inject rgb into final palette
+					idxrgb[memDist[k][0]] = memDist[k][1];
+
+					palLen++;
+					k++;
+				}
+			}
+		}
+
+		var len = idxrgb.length;
+		for (var i = 0; i < len; i++) {
+			if (!idxrgb[i]) continue;
+
+			this.idxrgb.push(idxrgb[i]);
+			this.idxi32.push(idxi32[i]);
+
+			this.i32idx[idxi32[i]] = this.idxi32.length - 1;
+			this.i32i32[idxi32[i]] = idxi32[i];
+			this.i32rgb[idxi32[i]] = idxrgb[i];
+		}
+
+		// build cache
+		// TODO?: this caching should be based on freqHash (eg: all > 2, top 4096)
+		for (var i32 in i32i32) {
+			var idx = this.nearestIndex(i32);
+
+			this.i32idx[i32] = idx;
+			this.i32i32[i32] = this.idxi32[idx];
+			this.i32rgb[i32] = this.idxrgb[idx];
+		}
+	};
+
+	// global top-population
+	RgbQuant.prototype.colorStats1D = function colorStats1D(buf32) {
+		var histG = this.histogram,
+			num = 0, col,
+			len = buf32.length;
+
+		for (var i = 0; i < len; i++) {
+			// skip transparent
+			if ((buf32[i] & 0xff000000) >> 24 == 0) continue;
+
+			// hack to retain stable order
+			col = buf32[i];
+
+			if (col in histG)
+				histG[col]++;
+			else
+				histG[col] = 1;
+		}
+	};
+
+	// population threshold within subregions
+	// FIXME: this can over-reduce (few/no colors same?), need a way to keep
+	// important colors that dont ever reach local thresholds (gradients?)
+	RgbQuant.prototype.colorStats2D = function colorStats2D(buf32, width) {
+		var boxW = this.boxSize[0],
+			boxH = this.boxSize[1],
+			area = boxW * boxH,
+			boxes = makeBoxes(width, buf32.length / width, boxW, boxH),
+			histG = this.histogram,
+			self = this;
+
+		boxes.forEach(function(box) {
+			var effc = Math.max(Math.round((box.w * box.h) / area) * self.boxPxls, 2),
+				histL = {}, col;
+
+			iterBox(box, width, function(i) {
+				// skip transparent
+				if ((buf32[i] & 0xff000000) >> 24 == 0) return;
+
+				// hack to retain stable order
+				col = buf32[i];
+
+				if (col in histG)
+					histG[col]++;
+				else if (col in histL) {
+					if (++histL[col] >= effc)
+						histG[col] = histL[col];
+				}
+				else
+					histL[col] = 1;
+			});
+		});
+	};
+
+	// TODO: group very low lum and very high lum colors
+	// TODO: pass custom sort order
+	RgbQuant.prototype.sortPal = function sortPal() {
+		var self = this;
+
+		this.idxi32.sort(function(a,b) {
+			var idxA = self.i32idx[a],
+				idxB = self.i32idx[b],
+				rgbA = self.idxrgb[idxA],
+				rgbB = self.idxrgb[idxB];
+
+			var hslA = rgb2hsl(rgbA[0],rgbA[1],rgbA[2]),
+				hslB = rgb2hsl(rgbB[0],rgbB[1],rgbB[2]);
+
+			// sort all grays + whites together
+			var hueA = (rgbA[0] == rgbA[1] && rgbA[1] == rgbA[2]) ? -1 : hueGroup(hslA.h, self.hueGroups);
+			var hueB = (rgbB[0] == rgbB[1] && rgbB[1] == rgbB[2]) ? -1 : hueGroup(hslB.h, self.hueGroups);
+
+			var hueDiff = hueB - hueA;
+			if (hueDiff) return -hueDiff;
+
+			var lumDiff = lumGroup(+hslB.l.toFixed(2)) - lumGroup(+hslA.l.toFixed(2));
+			if (lumDiff) return -lumDiff;
+
+			var satDiff = satGroup(+hslB.s.toFixed(2)) - satGroup(+hslA.s.toFixed(2));
+			if (satDiff) return -satDiff;
+		});
+
+		// sync idxrgb & i32idx
+		this.idxi32.forEach(function(i32, i) {
+			self.idxrgb[i] = self.i32rgb[i32];
+			self.i32idx[i32] = i;
+		});
+	};
+
+	// TOTRY: use HUSL - http://boronine.com/husl/
+	RgbQuant.prototype.nearestColor = function nearestColor(i32) {
+		var idx = this.nearestIndex(i32);
+		return this.idxi32[idx];
+	}
+
+	// TOTRY: use HUSL - http://boronine.com/husl/
+	RgbQuant.prototype.nearestIndex = function nearestIndex(i32) {
+		var col = this.i32i32[i32];
+		if (col) return this.i32idx[col];
+
+		var min = 1000,
+			idx,
+			rgb = [
+				(i32 & 0xff),
+				(i32 & 0xff00) >> 8,
+				(i32 & 0xff0000) >> 16,
+			],
+			len = this.idxrgb.length;
+
+		for (var i = 0; i < len; i++) {
+			var dist = colorDist(rgb, this.idxrgb[i]);
+
+			if (dist < min) {
+				min = dist;
+				idx = i;
+			}
+		}
+
+		return idx;
+	}
+
+	// Rec. 709 (sRGB) luma coef
+	var Pr = .2126,
+		Pg = .7152,
+		Pb = .0722;
+
+	// http://alienryderflex.com/hsp.html
+	function rgb2lum(r,g,b) {
+		return Math.sqrt(
+			Pr * r*r +
+			Pg * g*g +
+			Pb * b*b
+		);
+	}
+
+	// returns perceptual Euclidean color distance
+	function colorDist(rgb0, rgb1) {
+		var rd = (rgb1[0]-rgb0[0]),
+			gd = (rgb1[1]-rgb0[1]),
+			bd = (rgb1[2]-rgb0[2]),
+			d2 = Pr*rd*rd + Pg*gd*gd + Pb*bd*bd;
+
+		return d2/(255*255);
+	}
+
+	// http://rgb2hsl.nichabi.com/javascript-function.php
+	function rgb2hsl(r, g, b) {
+		var max, min, h, s, l, d;
+		r /= 255;
+		g /= 255;
+		b /= 255;
+		max = Math.max(r, g, b);
+		min = Math.min(r, g, b);
+		l = (max + min) / 2;
+		if (max == min) {
+			h = s = 0;
+		} else {
+			d = max - min;
+			s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+			switch (max) {
+				case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+				case g:	h = (b - r) / d + 2; break;
+				case b:	h = (r - g) / d + 4; break
+			}
+			h /= 6;
+		}
+//		h = Math.floor(h * 360)
+//		s = Math.floor(s * 100)
+//		l = Math.floor(l * 100)
+		return {
+			h: h,
+			s: s,
+			l: rgb2lum(r,g,b),
+		};
+	}
+
+	function hueGroup(hue, segs) {
+		var seg = 1/segs,
+			haf = seg/2;
+
+		if (hue >= 1 - haf || hue <= haf)
+			return 0;
+
+		for (var i = 1; i < segs; i++) {
+			var mid = i*seg;
+			if (hue >= mid - haf && hue <= mid + haf)
+				return i;
+		}
+	}
+
+	function satGroup(sat) {
+		return sat;
+		return Math.round(16 * sat);
+	}
+
+	function lumGroup(lum) {
+		return lum;
+		return Math.round(16 * lum);
+	}
+
+	function typeOf(val) {
+		return Object.prototype.toString.call(val).slice(8,-1);
+	}
+
+	var sort = isArrSortStable() ? Array.prototype.sort : stableSort;
+
+	// must be used via stableSort.call(arr, fn)
+	function stableSort(fn) {
+		var type = typeOf(this[0]);
+
+		if (type == "Number" || type == "String") {
+			var ord = {}, len = this.length, val;
+
+			for (var i = 0; i < len; i++) {
+				val = this[i];
+				if (ord[val] || ord[val] === 0) continue;
+				ord[val] = i;
+			}
+
+			return this.sort(function(a,b) {
+				return fn(a,b) || ord[a] - ord[b];
+			});
+		}
+		else {
+			var ord = this.map(function(v){return v});
+
+			return this.sort(function(a,b) {
+				return fn(a,b) || ord.indexOf(a) - ord.indexOf(b);
+			});
+		}
+	}
+
+	// test if js engine's Array#sort implementation is stable
+	function isArrSortStable() {
+		var str = "abcdefghijklmnopqrstuvwxyz";
+
+		return "xyzvwtursopqmnklhijfgdeabc" == str.split("").sort(function(a,b) {
+			return ~~(str.indexOf(b)/2.3) - ~~(str.indexOf(a)/2.3);
+		}).join("");
+	}
+
+	// returns uniform pixel data from various img
+	// TODO?: if array is passed, createimagedata, createlement canvas? take a pxlen?
+	function getImageData(img, width) {
+		var can, ctx, imgd, buf8, buf32, height;
+
+		switch (typeOf(img)) {
+			case "HTMLImageElement":
+				can = document.createElement("canvas");
+				can.width = img.width;
+				can.height = img.height;
+				ctx = can.getContext("2d");
+				ctx.drawImage(img,0,0);
+			case "HTMLCanvasElement":
+				can = can || img;
+				ctx = ctx || can.getContext("2d");
+			case "CanvasRenderingContext2D":
+				ctx = ctx || img;
+				can = can || ctx.canvas;
+				imgd = ctx.getImageData(0, 0, can.width, can.height);
+			case "ImageData":
+				imgd = imgd || img;
+				buf8 = imgd.data;
+				width = imgd.width;
+			case "Array":
+				buf8 = buf8 || new Uint8Array(img);
+			case "Uint8Array":
+			case "Uint8ClampedArray":
+				buf8 = buf8 || img;
+				buf32 = new Uint32Array(buf8.buffer);
+			case "Uint32Array":
+				buf32 = buf32 || img;
+				buf8 = buf8 || new Uint8Array(buf32.buffer);
+				width = width || buf32.length;
+				height = buf32.length / width;
+		}
+
+		return {
+			can: can,
+			ctx: ctx,
+			imgd: imgd,
+			buf8: buf8,
+			buf32: buf32,
+			width: width,
+			height: height,
+		};
+	}
+
+	// partitions a rect of wid x hgt into
+	// array of bboxes of w0 x h0 (or less)
+	function makeBoxes(wid, hgt, w0, h0) {
+		var wnum = ~~(wid/w0), wrem = wid%w0,
+			hnum = ~~(hgt/h0), hrem = hgt%h0,
+			xend = wid-wrem, yend = hgt-hrem;
+
+		var bxs = [];
+		for (var y = 0; y < hgt; y += h0)
+			for (var x = 0; x < wid; x += w0)
+				bxs.push({x:x, y:y, w:(x==xend?wrem:w0), h:(y==yend?hrem:h0)});
+
+		return bxs;
+	}
+
+	// iterates @bbox within a parent rect of width @wid; calls @fn, passing index within parent
+	function iterBox(bbox, wid, fn) {
+		var b = bbox,
+			i0 = b.y * wid + b.x,
+			i1 = (b.y + b.h - 1) * wid + (b.x + b.w - 1),
+			cnt = 0, incr = wid - b.w + 1, i = i0;
+
+		do {
+			fn.call(this, i);
+			i += (++cnt % b.w == 0) ? incr : 1;
+		} while (i <= i1);
+	}
+
+	// returns array of hash keys sorted by their values
+	function sortedHashKeys(obj, desc) {
+		var keys = [];
+
+		for (var key in obj)
+			keys.push(key);
+
+		return sort.call(keys, function(a,b) {
+			return desc ? obj[b] - obj[a] : obj[a] - obj[b];
+		});
+	}
+
+	// expose
+	this.RgbQuant = RgbQuant;
+
+}).call(this);
