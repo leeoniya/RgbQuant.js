2bb473253fcf4f5fa88b217fad714d73043dd9f0
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2023-08-08 19:06:51 -0300

    Refactor to use `async`.

--- index.js
+++ index.js
@@ -5,19 +5,22 @@ const Image = Canvas.Image;
 
 const { RgbQuantSMS } = require('./src/rgbquant-sms');
 
-const quant = new RgbQuantSMS({
-	colors: 16,
-	paletteCount: 1,
-	maxTiles: 256,
-	minHueCols: 0,
-	weighPopularity: true
-});
+(async () => {
+	const quant = new RgbQuantSMS({
+		colors: 16,
+		paletteCount: 1,
+		maxTiles: 256,
+		minHueCols: 0,
+		weighPopularity: true
+	});
 
-const getImg = async (src) => new Promise((resolve, reject) => {
-	const img = new Image();
-	img.onload = () => resolve(img);
-	img.onerror = reject;
-	img.src = src;
-});
+	const getImg = async (src) => new Promise((resolve, reject) => {
+		const img = new Image();
+		img.onload = () => resolve(img);
+		img.onerror = reject;
+		img.src = src;
+	});
 
-getImg('demo/img/biking.jpg').then(img => console.log('img', img));
+	const img = await getImg('demo/img/biking.jpg');
+	console.log('img', img)
+})();
